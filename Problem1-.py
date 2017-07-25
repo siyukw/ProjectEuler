b093876526959fdb02318784ff7df9318772f2e5
Siyu Kat Wang <siyukw@gmail.com>
2017-07-25 14:43:08 -0400

    done with 1-4 in Python

--- Problem1-.py
+++ Problem1-.py
@@ -28,18 +28,38 @@ while s is True:
         count = count - 1
         j = n
 print sum[Finished in 0.086s]
-'''
+
 #Problem 3
 check = True
 a = 600851475143
 i = 2
 while check:
     if a % i == 0:
-        print "yeah"
-        print i
         a = a / i
         if i >= a:
             print i
             check = False
     else:
         i = i + 1
+[Finished in 0.082s]
+'''
+# Problem 4
+a = 999
+b = 999
+hold = 0
+for i in range(100, 999):
+    a = i
+    for j in range(100, 999):
+        b = j
+        result = a * b
+        if result > 100000:
+            if (result // 100000 == result % 10) and ((result % 100000 / 10000 == result % 100 / 10) and (result % 10000 / 1000 == result % 1000 / 100)):
+                if result > hold:
+                    hold = result
+        else:
+            if (result // 10000 == result % 10) and (result % 10000 / 1000 == result % 1000 / 100):
+                if result > hold:
+                    hold = result
+
+print a, b
+print hold
